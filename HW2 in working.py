ded07ff6997c76220c440d77312e81e84319a4f0
Omer1 <omer.hadar96@gmail.com>
2022-04-17 21:12:46 +0300

    after changing the loop base on how long is the list

--- HW2 in working.py
+++ HW2 in working.py
@@ -7,26 +7,14 @@ def q2(input_file_name):
 
     input_file = open(input_file_name, "r")
     word_lst = input_file.read().split()  # List of the words in the file
-    longest_len = len(max(word_lst, key=len))
-    len_word = 1  # A variable of
     sorted_lst = []
-    while True:
-        for i in range(len(word_lst) + 1):
-            if i == len(word_lst):
-                len_word = len_word + 1
-            elif len(word_lst[i]) == len_word:
-                output_word = (word_lst[i], len_word)
-                sorted_lst.append(output_word)
-            elif len_word > longest_len:
-                input_file.close()
-                return False
-    output_address = "output_" + input_file_name
-    output_file = open(output_address, "w")
-    output_file.write("".join(sorted_lst))
-    output_file.close()
+    while len(word_lst) != 0:
+        shortest_word = min(word_lst, key=len)
+        word_added = (shortest_word, len(shortest_word))
+        sorted_lst.append(word_added)
+        word_lst.remove(shortest_word)
 
+    with open("output_" + input_file_name, "w") as output_sile:
+        for item in sorted_lst:
+            output_sile.write(str(item) + ", ")
 
-a = "check_1.txt"
-q2(a)
-f = open("output_check_1.text", "r")
-print(f.read())
