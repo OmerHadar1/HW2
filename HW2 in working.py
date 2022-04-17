e84959c78752d51cc9b6c393cef34fe047fa3031
Omer1 <omer.hadar96@gmail.com>
2022-04-17 20:45:50 +0300

    before changing the loop base on how long is the list

--- HW2 in working.py
+++ HW2 in working.py
@@ -1,27 +1,32 @@
-def q1(input_file_name):
+def q2(input_file_name):
     """
-    Read values from input file
-    :param input_file_name: An input file name
-    :return: The result of the division of the first number by the others
+      a. Read words from input file
+      b. Classify the words by length
+      c. Write each word and its length to output file.
     """
-    user_file = open(input_file_name, "r")
-    lst_text = user_file.read().split()
-    print(lst_text)
-    try:
-        div_num = float(lst_text[0])
-        for i in range(1, len(lst_text)):
-            div_num = div_num / float(lst_text[i])
-        print(div_num)
 
-    except FileNotFoundError:
-        print(-1)
-
-    except ZeroDivisionError:
-        print(-2)
-
-    except ValueError or TypeError:
-        print(-3)
+    input_file = open(input_file_name, "r")
+    word_lst = input_file.read().split()  # List of the words in the file
+    longest_len = len(max(word_lst, key=len))
+    len_word = 1  # A variable of
+    sorted_lst = []
+    while True:
+        for i in range(len(word_lst) + 1):
+            if i == len(word_lst):
+                len_word = len_word + 1
+            elif len(word_lst[i]) == len_word:
+                output_word = (word_lst[i], len_word)
+                sorted_lst.append(output_word)
+            elif len_word > longest_len:
+                input_file.close()
+                return False
+    output_address = "output_" + input_file_name
+    output_file = open(output_address, "w")
+    output_file.write("".join(sorted_lst))
+    output_file.close()
 
 
 a = "check_1.txt"
-q1(a)
+q2(a)
+f = open("output_check_1.text", "r")
+print(f.read())
